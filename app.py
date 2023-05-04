b4b65d16eb0cfd4e5d4b3754b192e876b6c3c829
alocke8181 <alocke_8181@yahoo.com>
2023-05-04 16:53:07 -0400

    completed 1-7

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, render_template, redirect, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+responses = []
+
+survey = satisfaction_survey
+num_questions = len(survey.questions)
+
+
+@app.route("/", methods=["GET","POST"])
+def show_start():
+    responses = []
+    return render_template("start.html", survey = survey)
+
+@app.route("/start", methods=["POST"])
+def start_survey():
+    return redirect("/questions/0")
+
+@app.route("/questions/<int:q_num>")
+def show_question(q_num):
+    if q_num == num_questions:
+        return redirect("/results")
+    if (responses is None):
+        return redirect("/")
+    if (q_num != len(responses)):
+        flash(f'Invalid question {q_num}, redirecting...')
+        return redirect(f'/questions/{len(responses)}')
+    else:
+        return render_template("question.html", curr_question = survey.questions[q_num])
+
+@app.route("/question_submit", methods=["POST"])
+def add_answer():
+    answer = request.form['answer']
+    responses.append(answer)
+    return redirect(f'/questions/{len(responses)}')
+
+@app.route("/results")
+def show_results():
+    return render_template("results.html", survey = survey, responses = responses)
+
+
